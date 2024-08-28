af023742e386cd5e42ec23db661c6f5eec5e5b20
RugeFX <zackfxg@gmail.com>
2024-08-29 00:03:44 +0700

    feat: trying out squeelecks, dotenv stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,21 @@
+use std::env;
+
 use axum::{
+    extract::State,
     http::{Method, StatusCode},
     response::Html,
     routing::{get, post},
     Json, Router,
 };
+use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tower_http::cors::{Any, CorsLayer};
+use tracing_subscriber::fmt::format;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().expect(".env file not detected!");
     tracing_subscriber::fmt::init();
 
     let cors = CorsLayer::new()
@@ -21,21 +28,48 @@ async fn main() {
         ])
         .allow_origin(Any);
 
+    let db_url = env::var("DATABASE_URL")?;
+
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&db_url)
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&db)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
     let app = Router::new()
         .route("/", get(html))
         .route("/api", get(root).post(echo))
-        .layer(cors);
+        .route("/query", get(query_test))
+        .layer(cors)
+        .with_state(db);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     println!("App listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
 #[derive(Serialize)]
 struct User {
     id: u64,
     username: String,
+}
+
+async fn query_test(State(db): State<PgPool>) -> String {
+    let result = sqlx::query("SELECT * FROM todos")
+        .fetch_all(&db)
+        .await
+        .unwrap();
+
+    format!("Todo : {:?}", result)
 }
 
 async fn html() -> Html<&'static str> {
